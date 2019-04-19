28d0da4c5a3af83d6f4306d086afda5c3de72167
Behzad Bagheri Behrouz <behzadb@live.com>
2019-04-19 19:17:55 +0430

    Add Identity Entities To Project

---
+++ Maxim.Entities/Identity/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using Maxim.Entities.AuditableEntity;
+using Microsoft.AspNetCore.Identity;
+
+namespace Maxim.Entities.Identity
+{
+    /// <summary>
+    /// More info: http://www.dotnettips.info/post/2577
+    /// and http://www.dotnettips.info/post/2578
+    /// </summary>
+    public class UserRole : IdentityUserRole<int>, IAuditableEntity
+    {
+        public virtual User User { get; set; }
+
+        public virtual Role Role { get; set; }
+    }
+}
---
+++ Maxim.Entities/Identity/UserUsedPassword.cs
@@ -0,0 +1,14 @@
+﻿using Maxim.Entities.AuditableEntity;
+
+namespace Maxim.Entities.Identity
+{
+    public class UserUsedPassword : IAuditableEntity
+    {
+        public int Id { get; set; }
+
+        public string HashedPassword { get; set; }
+
+        public virtual User User { get; set; }
+        public int UserId { get; set; }
+    }
+}
